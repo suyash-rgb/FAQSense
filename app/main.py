bd80b3c7c3712e00fd78cc936b8c632995c9b63d
Suyash Baoney <suyashbaoney58@gmail.com>
2026-02-21 17:55:52 +0530

    initial commit

---
+++ app/main.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+
+app = FastAPI(title="FAQSense Backend")
+
+@app.get("/")
+async def root():
+    return {"message": "Welcome to FAQSense Backend Servers"}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
